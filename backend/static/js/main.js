2d9e19d7fb1d7311f82021def331e334dde1bdde
Ecio <eciopai@gmail.com>
2025-06-06 09:37:23 -0300

    Primeiro commit: Sistema de avaliação Dialize Betim

---
+++ backend/static/js/main.js
@@ -0,0 +1,9 @@
+// Configuração do Lightbox
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'albumLabel': 'Imagem %1 de %2',
+    'fadeDuration': 300,
+    'imageFadeDuration': 300,
+    'positionFromTop': 50
+});
